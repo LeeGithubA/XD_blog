2d5b3c547906824da9e019fe20e7490a63f6b986
LiZhi <li.zh.mailcount@gmail.com>
2019-07-31 17:20:40 +0800

    initial

--- src/main/java/indi/lzh/XD_blog/index/controller/IndexController.java
+++ src/main/java/indi/lzh/XD_blog/index/controller/IndexController.java
@@ -1,5 +1,7 @@
 package indi.lzh.XD_blog.index.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class IndexController {
+    private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
     @RequestMapping("/index")
     public String index() {
         return "Hello World";
